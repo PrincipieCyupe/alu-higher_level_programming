4a284856ab6daa27fbd875c5a9ba3cf5045c4de6
PrincipieCyupe <p.cyubahiro@alustudent.com>
2024-11-23 17:43:24 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""
-This script fetches a URL's status using the urllib package.
-"""
+"""Documented by Lsblack"""
 import urllib.request
 
-if __name__ == '__main__':
-    # URL to fetch - update it based on the required test case
-    url = 'https://alu-intranet.hbtn.io/status'
+url = 'https://intranet.hbtn.io/status'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
+    '\n    Chrome/99.0.4844.84 Safari/537.36',
+}
 
-    # Create a request with a User-Agent header to avoid HTTP 403 errors
-    request = urllib.request.Request(
-        url, headers={'User-Agent': 'Mozilla/5.0'}
-    )
-
-    # Using 'with' to handle the URL response
-    with urllib.request.urlopen(request) as response:
-        content = response.read()
-
-        # Print the output in the required format
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+req = urllib.request.Request(url, headers=headers)
+with urllib.request.urlopen(req) as response:
+    content = response.read()
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", content.decode("utf-8"))
