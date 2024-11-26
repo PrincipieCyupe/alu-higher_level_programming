a28372db1aa3ae9ad47f2bbc456c76858f03f7c5
PrincipieCyupe <p.cyubahiro@alustudent.com>
2024-11-26 09:55:39 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     res = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))i
+    print("\t- content: {}".format(res.text))
