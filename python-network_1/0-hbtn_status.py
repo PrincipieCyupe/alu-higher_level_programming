2d402c280bdb42f9f5e08ac4fc2ae4fadc4ee802
PrincipieCyupe <p.cyubahiro@alustudent.com>
2024-11-26 09:42:06 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that fetches http://0.0.0.0:5050/status
-- using the urllib package.
 """
+Documentation
+Fetches data from the url using
+the urllib module in python
+"""
+
 import urllib.request
 
-if __name__ == '__main__':
-    url = 'http://0.0.0.0:5050/status'
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
+
+if _name_ == '_main_':
+    with urllib.request.urlopen(url) as f:
+        content = f.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
