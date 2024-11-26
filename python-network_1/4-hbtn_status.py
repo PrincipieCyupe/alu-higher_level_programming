5503dc9786d58bb2ddcbafd922badfbb5dec4d3f
PrincipieCyupe <p.cyubahiro@alustudent.com>
2024-11-26 09:49:37 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that
-- fetches https://intranet.hbtn.io/status.
+"""Fetches url
+using the request moduel
 """
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://alu-intranet.hbtn.io/status")
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))i
