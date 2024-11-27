dd978668ce14689daeb890adfa0141c0905c4eee
PrincipieCyupe <p.cyubahiro@alustudent.com>
2024-11-27 04:25:34 -0800

    other resolved

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""__summary__
-- Write a Python script that
-- fetches https://intranet.hbtn.io/status.
-=======
-"""Fetches url
-using the request module
->>>>>>> a28372db1aa3ae9ad47f2bbc456c76858f03f7c5
+"""
+Fetches a URL using the requests module.
+Supports fetching both https://intranet.hbtn.io/status and
+https://alu-intranet.hbtn.io/status based on conditions.
 """
 import requests
 
-
 if __name__ == "__main__":
-<<<<<<< HEAD
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
-=======
-    url = 'https://intranet.hbtn.io/status'
-    if url.startswith('https://'):
+    url = "https://intranet.hbtn.io/status"
+    # Adjust URL if necessary
+    if url.startswith("https://"):
         url = "https://alu-intranet.hbtn.io/status"
-    res = requests.get(url)
+    
+    response = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
->>>>>>> a28372db1aa3ae9ad47f2bbc456c76858f03f7c5
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
+
