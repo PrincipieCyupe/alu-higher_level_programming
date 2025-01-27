26009181a5835b0ced5ec8854abeefd9242dc039
PrincipieCyupe <p.cyubahiro@alustudent.com>
2025-01-27 14:36:40 -0800

    7-multi_c

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,5 +2,7 @@
 const argv = process.argv.slice(2);
 const txt = parseInt(argv[0]);
 const str = 'C is fun';
-if ( isNaN(txt) ) { console.log('Missing number of occurrences'); }
-else { let i = 0; while (i < txt) { console.log(str); i++; } }
+if (isNaN(txt)) { console.log('Missing number of occurrences'); }
+else {
+  let i = 0; while (i < txt) { console.log(str); i++; } 
+}
