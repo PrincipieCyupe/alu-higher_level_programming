c902dd49325a57eb0e08e6d08b1c817dd47392d6
PrincipieCyupe <p.cyubahiro@alustudent.com>
2025-01-27 13:49:47 -0800

    6-multi_languages_loop

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const teetfile = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (i = 0; i < teetfile.length; i++){console.log(teetfile[i])};
+for (const textfile of teetfile){console.log(textfile)};
