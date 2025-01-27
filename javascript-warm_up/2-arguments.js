299b987fa9995d30af624348f0eadb0548a9a7ff
PrincipieCyupe <p.cyubahiro@alustudent.com>
2025-01-27 11:59:59 -0800

    2-arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const argument_vector = process.argv.slice(2);
-const scrii = argument_vector.length > 0 ? (argument_vector.length === 1 ? "Argument found" : "Arguments found") : "no Argument";
+const scrii = argument_vector.length <= 1 ? (argument_vector.length === 1 ? 'Argument found' : 'No argument') : 'Arguments found';
 console.log(scrii);
