6b26c20a207fe09ef47de5517c7c57fc3b4a5c57
PrincipieCyupe <p.cyubahiro@alustudent.com>
2025-01-30 07:18:41 -0800

    8-square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 const argv = process.argv.slice(2);
 const txt = parseInt(argv[0]);
 if (!isNaN(txt)) {
-	for (let i = 0; i < txt; i++) { console.log('X'.repeat(txt)); }
+  for (let i = 0; i < txt; i++) { console.log('X'.repeat(txt)); }
 } else {
-	console.log('Missing size');
+  console.log('Missing size');
 }
