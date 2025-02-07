6d044c40514dab601ed30cda21327eeded5772d9
Methode <methode1d@gmail.com>
2025-02-07 10:48:35 +0200

    0-readme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-    console.log(err);
+    console.error(err); // Use console.error for error messages
     return;
   }
-  console.log(data);
+  process.stdout.write(data); // Use process.stdout.write to avoid extra new lines
 });
